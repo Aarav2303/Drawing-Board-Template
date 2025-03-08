7be37db4ef8c6577ac4db0d0561a24e7fe3d6876
Aarav2303 <aaravsinha.9th@gmail.com>
2025-03-08 16:39:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,42 +1,56 @@
-const canvas=document.getElementById("drawingBoard");
-const ctx=canvas.getContext("2d");
-//Setting canvas size
-canvas.width=window.innerWidth-20;
-canvas.height=window.innerHeight-100;
-//default brush settings
-let painting=false;
-let brushColor="#000000";
-let brushSize=5;
-//Mouse settings
-canvas.addEventListener("mousedown",()=> painting=true);
-canvas.addEventListener("mouseup",()=> painting=false);
-canvas.addEventListener("mousemove",draw);
-//Function to draw on canvas
-function draw(event){
-  if(!painting) return;
-  ctx.lineWidth=brushSize;
-  ctx.lineCap="round";
-  ctx.strokeStyle=brushColor;
-  ctx.lineTo(event.clientX-canvas.offsetLeft,event.clientY-canvas.offetTop);
+const canvas = document.getElementById("drawingBoard");
+const ctx = canvas.getContext("2d");
+
+// Setting canvas size
+canvas.width = window.innerWidth - 20;
+canvas.height = window.innerHeight - 100;
+
+// Default brush settings
+let painting = false;
+let brushColor = "#000000";
+let brushSize = 5;
+
+// Mouse events
+canvas.addEventListener("mousedown", () => (painting = true));
+canvas.addEventListener("mouseup", () => (painting = false));
+canvas.addEventListener("mousemove", draw);
+
+// Function to draw on canvas
+function draw(event) {
+  if (!painting) return;
+  
+  ctx.lineWidth = brushSize;
+  ctx.lineCap = "round";
+  ctx.strokeStyle = brushColor;
+  
+  ctx.lineTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
+  ctx.stroke();
+  
+  ctx.beginPath();
+  ctx.moveTo(event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop);
 }
-//Color Picker
-document.getElementById("colorPicker").addEventListener("input",(e)=> {
-  brushColor=e.target.value;
+
+// Color Picker
+document.getElementById("colorPicker").addEventListener("input", (e) => {
+  brushColor = e.target.value;
 });
-//Brush Size
-document.getElementById("brushSize").addEventListener("click",()=> {
-  brushSize=e.target.value;
+
+// Brush Size
+document.getElementById("brushSize").addEventListener("input", (e) => {
+  brushSize = e.target.value;
 });
-//Clear Canvas
-document.getElementById("clearCanvas").addEventListener("click",()=> {
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+
+// Clear Canvas
+document.getElementById("clearCanvas").addEventListener("click", () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 });
-//Save Image
-document.getElementById("saveElement").addEventListener("click",()=> {
-  const link=document.creatElement("a");
-link.download="drawing.png";
-link.href=canvas.toDataURL();
-link.click();
+
+// Save Image
+document.getElementById("saveImage").addEventListener("click", () => {
+  const link = document.createElement("a");
+  link.download = "drawing.png";
+  link.href = canvas.toDataURL();
+  link.click();
 });
 
 
