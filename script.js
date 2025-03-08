e4d2e98e4fa111650b4a045285edc45165d7a4f2
Aarav2303 <aaravsinha.9th@gmail.com>
2025-03-08 16:08:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -17,3 +17,35 @@ function draw(event){
   ctx.lineWidth=brushSize;
   ctx.lineCap="round";
   ctx.strokeStyle=brushColor;
+  ctx.lineTo(event.clientX-canvas.offsetLeft,event.clientY-canvas.offetTop);
+}
+//Color Picker
+document.getElement("colorPicker").addEventListener("input",(e)=> {
+  brushColor=e.target.value;
+});
+//Brush Size
+document.getElement("brushSize").addEventListener("click",()=> {
+  brushSize=e.target.value;
+});
+//Clear Canvas
+document.getElement("clearCanvas").addEventListener("click",()=> {
+  ctx.clearRect(0,0,canvas.width,canvas.height);
+});
+//Save Image
+document.getElement("saveElement").addEventListener("click",()-> {
+  const link=document.clearElement("a");
+link.download="drawing.png";
+link.href=canvas.toDataURL();
+link.click();
+});
+
+
+
+
+
+
+
+
+
+
+
